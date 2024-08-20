9744cb78ae23df7919ce1fdc7d8b5f3299b2a5c6
Nicolas Braun <n.braun1011@gmail.com>
2024-08-20 08:38:20 +0200

    commit

--- Main.java
+++ Main.java
@@ -5,21 +5,44 @@ public class Main {
         Motorbike bike = new Motorbike(212, "BMW");
         Bicycle bicycle = new Bicycle("Orbea");
 
+        System.out.println("---Inheritance---");
         bike.moveForward();
 
         //Subtype Polymorphism
         useVehicle(car);
         useVehicle(bicycle);
+
+        //Parametric Polymorphism
+        System.out.println("---Polymorphism---");
+        Box<Integer> intBox = new Box<>(5);
+        Box<String> stringBox = new Box<>("Hello");
+        System.out.println(intBox.getValue());
+        System.out.println(stringBox.getValue());
+
+        //Ad-hoc Polymorphism
+        System.out.println(calculator(5));
+        System.out.println(calculator(25, 6));
     }
 
     static private void useVehicle(Vehicle vehicle) {
         System.out.println("I'm using the " + vehicle.getClass().getSimpleName());
+        vehicle.start();
     }
 
+    static private int calculator(int a) {
+        return a+a;
+    }
+
+    static private int calculator(int a, int b) {
+        return a+b;
+    }
+
+
+    //Inheritance
     static abstract class Vehicle {
-        int tires;
-        boolean hasEngine;
-        String brand;
+        private int tires;
+        private boolean hasEngine;
+        private String brand;
     
         public Vehicle(int tires, boolean hasEngine, String brand) {
             this.tires = tires;
@@ -28,6 +51,10 @@ public class Main {
         }
     
         public abstract void start();
+
+        public String getBrand() {
+            return this.brand;
+        }
     
         public void moveForward() {
             System.out.println("Vehicle moves forward");
@@ -85,6 +112,20 @@ public class Main {
     static class Bicycle extends NonMotorizedVehicle {
         public Bicycle(String brand) {
             super(2, brand);
+        }
+    }
+
+
+    /* Parametric Polymorphism */
+    static class Box<T> {
+        private T value;
+
+        public Box(T value) {
+            this.value = value;
+        }
+
+        public T getValue() {
+            return value;
         }
     }
 }
